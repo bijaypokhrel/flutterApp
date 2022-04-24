148625ef3492860ae2d22e08351dc98f9a2b8e5b
Bijay Pokhrel <pokhrelbijay44@gmail.com>
2022-04-24 10:44:02 +0545

    Upto Day 4

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+import 'login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => const HomePage(),
+        "/login": (context) => const LoginPage(),
+      },
+    );
+  }
+}
